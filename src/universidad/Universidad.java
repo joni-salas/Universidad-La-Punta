0d6a1cc4a40466e13480b8717bb5162bdd2f91e7
Gaston <Hyouka21@github.com>
2020-10-22 17:57:07 -0300

    ultimas modificaciones

--- src/universidad/Universidad.java
+++ src/universidad/Universidad.java
@@ -2,6 +2,8 @@
 package universidad;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 import universidad.entidades.*;
 import universidad.modelo.*;
 
@@ -19,25 +21,33 @@ public class Universidad {
         //md.borrarMateria(3);
         //md.obtenerMaterias();
         // System.out.println(md.obtenerMaterias());
-        //InscripcionData id=new InscripcionData(c);
-        Materia mat=new Materia("quimica");
-        md.guardarMateria(mat);
-         
-        
-        
-        
+        InscripcionData id=new InscripcionData(c);
+         AlumnoData ad=new AlumnoData(c);
+       // Materia mat=new Materia("quimica");
+       // md.guardarMateria(mat);
+         //Alumno alum = ad.buscarAlumno(3);
+        // Materia mat = md.buscarMateria(2);
+         Inscripcion ins= new Inscripcion();
+         //id.guardarInscripcion(ins);
+         List<Inscripcion> arr = new ArrayList<>();
+         arr= id.obtenerInscripcion();
+         for(int x=0;x<arr.size();x++){
+             ins=arr.get(x);
+         System.out.println(ins.getAlumno().getNombreAlumno());
+         }
+
         //Pruebas Nahuel
         //Alumno nahuel=new Alumno("Nahuel Vargas",LocalDate.of(1998, 6, 13),false);
         //Alumno gaston=new Alumno("Gaston Sosa",LocalDate.of(1995, Month.JUNE, 19),true);
-        AlumnoData ad=new AlumnoData(c);
+       
        // ad.guardarAlumno(gaston);
         
         //ad.guardarAlumno(nahuel);
-        //ad.obtenerAlumnos();
-        //Alumno nahu = ad.buscarAlumno(11);
+        
+                //Alumno nahu = ad.buscarAlumno(11);
         //nahu.setFnAlumno(LocalDate.of(1998, 8, 13));
         //ad.actualizarAlumno(nahu);
-        ad.borrarAlumno(11);
+       // ad.borrarAlumno(11);
     }
     
 }
